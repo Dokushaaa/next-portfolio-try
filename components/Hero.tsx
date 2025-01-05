680f4237594985cd391cc2db714cbe27a3eafd79
Kiyo-Shikami <KiyoShikamiii@gmail.com>
2025-01-05 23:22:50 +0800

    ActernityUi dl-add

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { FaLocationArrow } from "react-icons/fa";
+import { Cover } from "./ui/TextCover";
 
 const Hero = ({
 	titleWords,
